b66dbcea01f2994dc31616c56d34fd074424111a
Isaac Adams <jiadams2@ncsu.edu>
2025-08-11 01:49:31 -0400

    initial project structure

---
+++ src/main/java/com/jiadams/rainbot/RainbotApplication.java
@@ -0,0 +1,13 @@
+package com.jiadams.rainbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainbotApplication.class, args);
+	}
+
+}
